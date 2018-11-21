30b780671d58a1fe8eb5560af0262973122a67d2
yousefgg <44702055+yousefgg@users.noreply.github.com>
2018-11-21 20:33:14 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,11 @@
+﻿bot.on("guildMemberAdd", async member => {
+  let moment2 = require('moment-duration-format'),
+      moment = require("moment"),
+      date = moment.duration(new Date() - member.user.createdAt).format("d");
+
+  if(date < 7) {
+    member.ban("Member account age is lower than 7 days.")
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
